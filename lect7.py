d7834810492addba70929b6b9329e86ca4266406
EC2 Default User <ec2-user@ip-172-31-69-8.ec2.internal>
2020-09-24 19:16:18 +0000

    Display pic multiple times using random number

--- lect7.py
+++ lect7.py
@@ -1,13 +1,16 @@
 import flask
 import os
+import random
 
 app = flask.Flask(__name__)
 
 
 @app.route('/') # Python decorator 
 def index(): 
+    num = random.randint(1, 4)
     return flask.render_template(
-        "index.html")
+        "index.html",
+        length = num)
 
 app.run(
     port=int(os.getenv('PORT', 8080)),
